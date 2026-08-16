13a98cba813fb11fc7e472e914a3fed41f367a0b
jimmy86gb <jimmy86gbgmail.com>
2026-08-16 14:02:53 -0500

    Version 1 Del Sistema

---
+++ 1ER_CORTE/2_Implementacion_de_Arboles_de_Busqueda_Externa/Proyectos/Arboles/src/main/java/com/mycompany/arboles/nodobm.java
@@ -0,0 +1,17 @@
+package com.mycompany.arboles;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class nodobm {
+    public List<Integer> dato;
+    public List<Nodo> children;
+    public Nodo next;
+    public nodobm(boolean hoja){
+        
+        
+    }
+    
+    
+    
+}
